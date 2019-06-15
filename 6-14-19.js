7e5ae0bcee836685f02b31379eb6ca93b7356758
github username <github email address>
2019-06-14 17:16:32 -0700

    Split Linked Lists

---
+++ 6-14-19.js
@@ -0,0 +1,17 @@
+// * Linked Lists
+
+// ** Split 
+function split(nums){
+    let currentNode = nums.head;
+    while(currentNode.next){
+      if(currentNode.next.value < 0){
+        let temp = currentNode.next;
+        currentNode.next = temp.next;
+        temp.next = nums.head;
+        nums.head =temp
+      } else {
+        currentNode = currentNode.next
+      }
+    } nums.tail = currentNode
+    return nums
+  }
