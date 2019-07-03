c5b3181b3152341fb3290b8447604e28a974b72a
github username <github email address>
2019-07-03 13:07:29 -0700

    Reverse String

--- 6-14-19.js
+++ 6-14-19.js
@@ -1,17 +1,18 @@
 // * Linked Lists
 
-// ** Split 
-function split(nums){
-    let currentNode = nums.head;
-    while(currentNode.next){
-      if(currentNode.next.value < 0){
-        let temp = currentNode.next;
-        currentNode.next = temp.next;
-        temp.next = nums.head;
-        nums.head =temp
-      } else {
-        currentNode = currentNode.next
-      }
-    } nums.tail = currentNode
-    return nums
+// ** Split
+function split(nums) {
+  let currentNode = nums.head;
+  while (currentNode.next) {
+    if (currentNode.next.value < 0) {
+      let temp = currentNode.next;
+      currentNode.next = temp.next;
+      temp.next = nums.head;
+      nums.head = temp;
+    } else {
+      currentNode = currentNode.next;
+    }
   }
+  nums.tail = currentNode;
+  return nums;
+}
